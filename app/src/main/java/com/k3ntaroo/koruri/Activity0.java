4d00873b7d93a26aef5a003884a137beb1728425
Kentaro Takakuwa <kentaro.tkkw@gmail.com>
2017-07-01 01:12:31 +0900

    1st commit with initial android project

---
+++ app/src/main/java/com/k3ntaroo/koruri/Activity0.java
@@ -0,0 +1,13 @@
+package com.k3ntaroo.koruri;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Activity0 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_0);
+    }
+}
